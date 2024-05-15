8f67d9f1b5a98a1beaf9bb297f653a2fa1eb2f6e
Slavkoo01 <134085710+Slavkoo01@users.noreply.github.com>
2024-05-15 23:30:29 +0200

    feat checkpoint

--- NetworkService/NetworkService/NetworkService/ViewModel/MeasurementViewModel.cs
+++ NetworkService/NetworkService/NetworkService/ViewModel/MeasurementViewModel.cs
@@ -10,8 +10,11 @@ namespace NetworkService.ViewModel
     public class MeasurementViewModel : ViewModelBase   
     {
         private ServerViewModel _selectedServer = new ServerViewModel();
-        public ServerViewModel SelectedServer {  get { return _selectedServer; } set { _selectedServer = value; OnPropertyChanged(nameof(SelectedServer)); } }
-
+        public ServerViewModel SelectedServer {  get { return _selectedServer; } set { _selectedServer = value; OnPropertyChanged(nameof(SelectedServer)); OnPropertyChanged(nameof(SelectedServerValue1)); } }
+        public double SelectedServerValue1
+        {
+            get { return SelectedServer?.Value1 ?? 0; }
+        }
         public ObservableCollection<ServerViewModel> MeasurementCollection { get; set; } = EntitiesViewModel.EntityColection;
 
         public MeasurementViewModel()
