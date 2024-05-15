b1ef351ab09f0b812e0baf748ec3e86955bc448c
Slavkoo01 <134085710+Slavkoo01@users.noreply.github.com>
2024-05-15 23:43:05 +0200

    feat animation for graph

--- NetworkService/NetworkService/NetworkService/Helper/AnimationHelper.cs
+++ NetworkService/NetworkService/NetworkService/Helper/AnimationHelper.cs
@@ -1,12 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
-namespace NetworkService.Helper
+public static class AnimationHelper
 {
-    internal class AnimationHelper
+    public static readonly DependencyProperty TriggerOnHeightChangeProperty =
+        DependencyProperty.RegisterAttached(
+            "TriggerOnHeightChange",
+            typeof(bool),
+            typeof(AnimationHelper),
+            new PropertyMetadata(false, OnTriggerOnHeightChange));
+
+    public static bool GetTriggerOnHeightChange(DependencyObject obj)
     {
+        return (bool)obj.GetValue(TriggerOnHeightChangeProperty);
+    }
+
+    public static void SetTriggerOnHeightChange(DependencyObject obj, bool value)
+    {
+        obj.SetValue(TriggerOnHeightChangeProperty, value);
+    }
+
+    private static void OnTriggerOnHeightChange(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is Rectangle rectangle && (bool)e.NewValue)
+        {
+            rectangle.SizeChanged += (sender, args) =>
+            {
+                var animation = new DoubleAnimation
+                {
+                    Duration = TimeSpan.FromSeconds(0.5),
+                    From = 30,
+                    To = rectangle.ActualHeight, // Assumes Height is set to Auto or changes dynamically
+                    EasingFunction = new QuadraticEase()
+                };
+                rectangle.BeginAnimation(Rectangle.HeightProperty, animation);
+            };
+        }
     }
 }
--- NetworkService/NetworkService/NetworkService/ViewModel/MeasurementViewModel.cs
+++ NetworkService/NetworkService/NetworkService/ViewModel/MeasurementViewModel.cs
@@ -11,6 +11,29 @@ namespace NetworkService.ViewModel
     {
         private ServerViewModel _selectedServer = new ServerViewModel();
         public ServerViewModel SelectedServer {  get { return _selectedServer; } set { _selectedServer = value; OnPropertyChanged(nameof(SelectedServer)); OnPropertyChanged(nameof(SelectedServerValue1)); } }
+        
+        public static event EventHandler Value1Changed;
+
+        private double _value1;
+
+        public double Value1
+        {
+            get => _value1;
+            set
+            {
+                if (_value1 != value)
+                {
+                    _value1 = value;
+                    OnPropertyChanged(nameof(Value1));
+                    OnValue1Changed();
+                }
+            }
+        }
+
+        protected virtual void OnValue1Changed()
+        {
+            Value1Changed?.Invoke(this, EventArgs.Empty);
+        }
         public double SelectedServerValue1
         {
             get { return SelectedServer?.Value1 ?? 0; }
