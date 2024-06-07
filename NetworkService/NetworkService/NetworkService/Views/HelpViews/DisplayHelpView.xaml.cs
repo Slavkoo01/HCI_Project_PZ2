1903f204e072f618cdf4ce4ffb063d8046e7507e
Slavkoo01 <134085710+Slavkoo01@users.noreply.github.com>
2024-06-07 14:26:19 +0200

    feat: GOTOVO

--- NetworkService/NetworkService/NetworkService/Views/HelpViews/DisplayHelpView.xaml.cs
+++ NetworkService/NetworkService/NetworkService/Views/HelpViews/DisplayHelpView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NetworkService.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,8 +21,10 @@ namespace NetworkService.Views.HelpViews
     /// </summary>
     public partial class DisplayHelpView : UserControl
     {
+     
         public DisplayHelpView()
         {
+            DataContext = this;
             InitializeComponent();
         }
     }
